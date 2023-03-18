faf7c635ddd8f3273b2df0c31a95da217aa6fdad
Abdullah <abdullah@gmail.com>
2023-03-17 18:04:58 -0700

    prof

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+for (i = 'a' : i <= 'z' : i++)
+puchar(i);
+putcher('\n');
+return (0);
+}
