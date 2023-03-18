752b7ce05f8514d4461ccec119aa8c92b08b4a52
Abdullah <abdullah@gmail.com>
2023-03-17 18:22:00 -0700

    prof

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+if (i != 'q' && i != 'e')
+putchar(i);
+putchar('\n');
+return (0);
+}
+
