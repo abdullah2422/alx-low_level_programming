1fee56a41da1e7083092d13cae47d7f225ebca4d
Abdullah <abdullah@gmail.com>
2023-03-17 18:32:10 -0700

    prof

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+int i = 0;
+while (i <= 9)
+	printf("%i", i);
+	i++;
+printf("\n");
+return (0);
+}
+
