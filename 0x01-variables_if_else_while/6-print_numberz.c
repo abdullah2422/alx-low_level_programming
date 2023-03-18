ddd7d30d5e811e82a970a46b67aded39f53111aa
Abdullah <abdullah@gmail.com>
2023-03-17 18:42:13 -0700

    prof

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+/*conver digit to ASCII repersentation*/
+putchar(digit, '0');
+digit++;
+}
+printf("\n");
+return (0);
+}
