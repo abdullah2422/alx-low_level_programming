efe8eeca9adb81ce6bec6bcd2cea616eebec359d
Abdullah <abdullah@gmail.com>
2023-03-17 18:49:33 -0700

    prof

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+char ch = 'z';
+while (ch >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+return (0);
+}
