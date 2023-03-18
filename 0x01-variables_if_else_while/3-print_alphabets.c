8f849d7e1fa2dacdd1f5174c5666899f19588b93
Abdullah <abdullah@gmail.com>
2023-03-17 18:18:30 -0700

    prof

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+for (i = 'A' ; i <= 'Z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
+
