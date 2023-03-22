55d5d373191dbbfe21788fd45cfe615d5b215177
Abdullah <abdullah@gmail.com>
2023-03-22 16:25:08 -0700

    abdo

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: 0
+*/
+void print_times_table(int n) {
+    for (int i = 0; i <= 10; i++) {
+        printf("%d x %d = %d\n", i, n, i * n);
+    }
+}
