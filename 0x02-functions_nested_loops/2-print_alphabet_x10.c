b34f0cd1ab2ed60a8389328c441297a964aac692
Abdullah <abdullah@gmail.com>
2023-03-21 18:08:28 -0700

    abdo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,15 @@
 /**
  * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
- * Return: x10 a-z
- */
+*/
 void print_alphabet_x10(void)
 {
-int line, ch;
-for (line = 0; line <= 9; line++)
-{
-for(ch = 'a'; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
-}
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for(ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
