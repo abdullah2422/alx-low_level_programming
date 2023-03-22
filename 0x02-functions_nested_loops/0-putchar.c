88787e3c9adfee503f545fd4ed2d5a73b727446a
Abdullah <abdullah@gmail.com>
2023-03-21 17:23:10 -0700

    abdo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,12 @@
  * Return: 0
 */
 int main(void)
-	char str[] = "_putchar"
+{
+	char str[] = "_putchar";
 	int ch;
-	for (ch = 0; ch < 5; ch++)
-		_putchar(str[ch];
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
 	_putchar('\n');
+	return (0);
+}
