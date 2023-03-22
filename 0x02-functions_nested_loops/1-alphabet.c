746912ef25da009f538fadddff50b09223b41615
Abdullah <abdullah@gmail.com>
2023-03-21 17:38:55 -0700

    abdo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+void print_alphabet(void)
+
+{
+        int ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+                _putchar(ch);
+        _putchar('\n');
+}
+
