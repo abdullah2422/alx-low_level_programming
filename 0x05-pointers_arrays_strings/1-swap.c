bbf9665e4b35b9cc41a4a7102321876885dce63e
Abdullah <abdullah@gmail.com>
2023-03-27 14:51:46 -0700

    ptr

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(&a, &b);
+{
+	*a = 42;
+	*b = 98;
+}
