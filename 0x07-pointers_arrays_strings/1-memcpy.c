22af55bacbecc7cf8ea6d485aed1733424acbc18
Abdullah <abdullah@gmail.com>
2023-04-03 15:17:52 -0700

    ptr

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,24 +10,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i % 10)
-        {
-            _putchar(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            _putchar("\n");
-        }
-        _putchar("0x%02x", buffer[i]);
-        i++;
-    }
-    _putchar("\n");
-}
-
-
-
-
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
 }
