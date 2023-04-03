881c923e16b27adc91d41c827b4cf94a3a96686b
Abdullah <abdullah@gmail.com>
2023-04-03 13:56:41 -0700

    ptr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,12 +3,13 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
- * Return: Nothing.
+ *  
+ * Return: putchar
  */
 char *_memset(char *s, char b, unsigned int n) {
-    for (unsigned int i = 0; i < n; i++) {
+	unsigned int i;
+    for (i = 0; i < n; i++) {
         s[i] = b;
     }
-    return s;
+    return (s);
 }
