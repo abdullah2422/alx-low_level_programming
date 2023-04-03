937f1da7e42112ef8c3a6282c9ce42dff72deddf
Abdullah <abdullah@gmail.com>
2023-04-03 14:32:50 -0700

    ptr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return: putchar
+ * _memset -fills memory with a constant byte.
+ * @n: bytes of the memory area pointed to by
+ * @s: with the constant byte
+ * @b: Returns a pointer to the memory area
+ * Return: s.
 */
 char *_memset(char *s, char b, unsigned int n)
 {
