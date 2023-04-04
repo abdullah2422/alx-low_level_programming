8743adb29a2b87cac82d5b4204efe03c362620eb
Abdullah <abdullah@gmail.com>
2023-04-03 17:20:27 -0700

    ptr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,23 +5,24 @@
  * @s: which consist only of bytes from
  * Return: accept
  */
-unsigned int _strspn(char *s, char *accept) {
-    unsigned int i = 0;
-    char *p = s;
-    while (*p != '\0') {
-        char *q = accept;
-        while (*q != '\0') {
-            if (*p == *q) {
-                len++;
-                break;
-            }
-            q++;
-        }
-        if (*q == '\0') {
-            return i;
-        }
-        p++;
-    }
-    return i;
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0;
+char *p = s;
+while (*p != '\0') {
+char *q = accept;
+while (*q != '\0') {
+if (*p == *q) {
+i++;
+break;
+}
+q++;
+}
+if (*q == '\0') {
+return i;
+}
+p++;
+}
+return i;
 }
 
