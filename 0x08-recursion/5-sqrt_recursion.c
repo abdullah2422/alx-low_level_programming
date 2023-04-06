7a901c12960794614b449fa784984a69882addaa
Abdullah <abdullah@gmail.com>
2023-04-05 19:47:26 -0700

    ptr

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
+ * @n: number to calculate the square root of.
  *
  * Return: the resulting square.
  */
