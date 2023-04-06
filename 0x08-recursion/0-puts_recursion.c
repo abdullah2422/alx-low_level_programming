427d685c392e6b68d60836ae96937b6e6a79fbd1
Abdullah <abdullah@gmail.com>
2023-04-05 17:52:17 -0700

    rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
- *_puts_recursion _prints a string, followed by a new line.
+ *_puts_recursion -prints a string, followed by a new line.
  *@s: input
  *Return: Alawys 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-_putchar("Puts with recursion")
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+int main(void)
+{
+char str[] = "Puts with recursion";
+_puts_recursion(str);
+return (0);
 }
