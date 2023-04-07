42351df2aebc937515869f81762d0d9527cb0850
Abdullah <abdullah@gmail.com>
2023-04-06 18:16:45 -0700

    ptr

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+int i, j, len;
+len = strlen(s);
+for (i = 0, j = len - 1; i < j; i++, j--)
+{
+if (s[i] != s[j])
+{
+return (0);
+}
+}
+return (1);
+}
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+*/
+int main(void)
+{
+char str1[] = "racecar";
+char str2[] = "hello";
+char str3[] = "";
+printf("%d\n", is_palindrome(str1));
+printf("%d\n", is_palindrome(str2));
+printf("%d\n", is_palindrome(str3));
+return (0);
+}
