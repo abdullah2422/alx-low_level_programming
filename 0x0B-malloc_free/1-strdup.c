e1dd76810fd97068bce3231cc92ef01cc51458f1
Abdullah <abdullah@gmail.com>
2023-04-12 11:56:51 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,17 +6,19 @@
  * @str: char
  * Return: 0
  */
-char *_strdup(char *str)
-{
-char *copy;
-if (str == NULL)
-{
-return (NULL);
-}
-copy = malloc(strlen(str) + 1);
-if (copy == NULL) {
-return (NULL);
-}
-strcpy(copy, str);
-return copy;
+char* _strdup(char* str) {
+    if (str == NULL) {
+        return NULL;
+    }
+
+    size_t len = strlen(str) + 1;
+    char* copy = (char*)malloc(len * sizeof(char));
+
+    if (copy == NULL) {
+        return NULL;
+    }
+
+    memcpy(copy, str, len);
+
+    return copy;
 }
