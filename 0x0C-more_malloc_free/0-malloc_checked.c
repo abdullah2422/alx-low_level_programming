c188874869c3c792115362c2d5148b126895a44f
Abdullah <abdullah@gmail.com>
2023-04-13 09:10:21 -0700

    malc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+void print_string(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+}
+}
