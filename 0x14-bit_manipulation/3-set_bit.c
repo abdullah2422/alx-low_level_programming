85476cf85e14c7e9046e3c27ad1723ab36f65327
abdullah2422 <abdullah@gmail.com>
2023-05-04 19:24:50 -0700

    ptr

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,12 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask;
+	unsigned long int mask;
 
-    if (index >= sizeof(unsigned long int) * 8)
-        return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-    mask = 1 << index;
-    *n |= mask;
-    return (1);
+	mask = 1 << index;
+	*n |= mask;
+	return (1);
 }
