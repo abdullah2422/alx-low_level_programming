0c6e7df3231ca5bb22b265c12588d5ce50d49a8a
abdullah2422 <abdullah@gmail.com>
2023-05-08 16:06:52 -0700

    io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,19 +7,46 @@
  * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *FileOut;
-	if(*filename == '\0')
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		printf("Error in the name of file!\n");
-		exit(1);
+		free(buf);
+		return (0);
 	}
-	if((FileOut = fopen(filename, "a"))==NULL){
-		printf("Can't create file!\n");
-		exit(1);
-	}else{
-		fprintf(FileOut, "%s", letters);
+
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
 	}
-	fclose(FileOut);
+
+	buf[bytes_read] = '\0';
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+	return (bytes_written);
 }
+
