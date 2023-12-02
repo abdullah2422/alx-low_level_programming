e86ea8bf8f19a39e1104d00602b2c5c8b41c5577
abdullah2422 <abdullahmohammed1am@gmail.com>
2023-12-02 01:16:42 +0000

    python

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size:  the size of the array.
+ *
+ * Return: NULL.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *ht = malloc(sizeof(hash_table_t));
