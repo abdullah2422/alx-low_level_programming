66fb4175d59f3eaf1638e81255299c425b925e06
abdullah2422 <abdullahmohammed1am@gmail.com>
2023-12-02 01:19:08 +0000

    python

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_create - function that creates a hash table.
  * @size:  the size of the array.
@@ -7,17 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL) {
-        return NULL;
-    }
+	hash_table_t *n_table;
 
-    ht->size = size;
-    ht->array = calloc(size, sizeof(hash_node_t *));
-    if (ht->array == NULL) {
-        free(ht);
-        return NULL;
-    }
+	if (!size)
+		return (NULL);
+	n_table = malloc(sizeof(hash_table_t));
+	if (!n_table)
+		return (NULL);
 
-    return ht;
+	n_table->size = size;
+	n_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!n_table->array)
+	{
+		free(n_table);
+		return (NULL);
+	}
+	return (n_table);
 }
